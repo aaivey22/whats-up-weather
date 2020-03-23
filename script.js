c68a92401d66d4c201de748315ecf8e59ffa7f9a
aaivey22 <angelawings22@gmail.com>
2020-03-23 16:02:59 -0400

    got 5day forecast appending to elements on screen

--- script.js
+++ script.js
@@ -9,20 +9,36 @@ var fetchWeather = function () { // e is short for event
     var userCity = $("#cityInput").val() // grabs input value
 
     var city = userCity || 'richmond'; // if user doesn't provide city, default to 'Richmond'
-    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city},us&appid=26b725bced630f42f77f9f97edf3d53a`;
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${city},us&appid=26b725bced630f42f77f9f97edf3d53a`;
 
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
         console.log(response)
-        // $('#searchResult').text(response.city);
+            
+        var weatherData = [response.list[2], response.list[10], response.list[18], response.list[26], response.list[34]]
+        console.log(weatherData)
 
-        // for (var i = 0; i < response.length; i++) {
-        //     // var coinImage = $('<img>').attr('src', response[i].image)
-        //     var cityName = $('<p>').text(response[i].name);
-        //     console.log(cityName)
-        //     $('#searchResult').text(cityName);
-        // }
-    });
+
+            var cityName = response.city.name;
+            $('#searchResult').append(cityName);
+
+
+            for (var i = 0; i < weatherData.length; i++) {
+                var weatherItem = $('<div>').addClass('futureForecast')
+                var day = $('<h5>').text(weatherData[i].dt_txt.split(' ')[0])
+                var temp = $('<h5>').text(weatherData[i].main.temp)
+                var humidity = $('<h5>').text(weatherData[i].main.humidity)
+                // var weatherIcon = $('<i>').attr('id', weatherData[i].weather[0].icon)
+                
+                weatherItem.append(day, temp, humidity)
+                $('#searchResult').append(weatherItem)
+            }
+
+
+            // console.log(cityName)
+           
+    })
 }
+// var coinImage = $('<img>').attr('src', response[i].image)
