e34c2d1185f2b7480a2d61e70d6672bc9c28d39a
aaivey22 <angelawings22@gmail.com>
2020-03-23 23:40:54 -0400

    adding current weather and moment.js for current date

--- script.js
+++ script.js
@@ -23,37 +23,55 @@ $('#userSearch').submit(function (event) {
 var fetchWeather = function (e) { // e is short for event
 
     var userCity = $("#cityInput").val() // grabs user-input value
-
     var city = userCity || 'richmond'; // if user doesn't provide city, default to 'Richmond'
+
+
+    var currentURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city},{state}us&appid=26b725bced630f42f77f9f97edf3d53a`;
+    $.ajax({
+        url: currentURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response)
+
+        $('#cityDisplay').empty()
+        var cityName = response.name;
+        $('#cityDisplay').append(cityName);
+
+        // for (var i = 0; i < weatherData.length; i++)
+        var currentDate = moment().format("MMM Do YYYY");
+        var weatherContainer = $('<div>').addClass('currentForecast')
+        var currentDay = $('<h5>').text(currentDate).append('<img id="icon" src=http://openweathermap.org/img/w/' + response.weather[0].icon + '.png>')
+        var currentTemp = $('<h5>').text('Current Temperature: ' + response.main.temp + ' ' + '°F')
+        var currentHumidity = $('<h5>').text('Current Humidity: ' + response.main.humidity + ' ' + '%')
+        var currentWindSpeed = $('<h5>').text('Wind Speed: ' + response.wind.speed + ' MPH')
+        // var currentUv = 
+        
+        $('#searchResultCurrent').append(currentDay, currentTemp, currentHumidity, currentWindSpeed);
+    })
+
+
     var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${city},{state}us&appid=26b725bced630f42f77f9f97edf3d53a`;
-    // var currentURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city},{state}us&appid=26b725bced630f42f77f9f97edf3d53a`;
     $.ajax({
         url: forecastURL,
         method: 'GET'
     }).then(function (response) {
         // console.log(response)
-        $('#title').empty()
+        
+        
         $('.futureForecast').remove()
 
         var forecastData = [response.list[2], response.list[10], response.list[18], response.list[26], response.list[34]]
         // console.log(forecastData)
 
-        var cityName = response.city.name;
-        $('#title').append(cityName);
-
-
-        // let temperature = $('<p>').attr('id', 'temperature').text('Temperature: ' + res.main.temp + ' ' +  'F')
-
         for (var i = 0; i < forecastData.length; i++) {
 
-            var weatherItem = $('<div>').addClass('futureForecast')
-            var day = $('<h5>').text(forecastData[i].dt_txt.split(' ')[0])
-            var temp = $('<h5>').text('Temperature: ' + forecastData[i].main.temp + ' ' + 'F')
+            var forecastContainer = $('<div>').addClass('futureForecast')
+            var day = $('<h5>').text(forecastData[i].dt_txt.split(' ')[0]).append('<img id="icon" src=http://openweathermap.org/img/w/' + forecastData[i].weather[0].icon + '.png>')
+            var temp = $('<h5>').text('Temperature: ' + forecastData[i].main.temp + ' ' + '°F')
             var humidity = $('<h5>').text('Humidity: ' + forecastData[i].main.humidity + ' ' + '%')
-            var weatherIcon = $('<h5>').prepend('<img id="icon" src=http://openweathermap.org/img/w/' + forecastData[i].weather[0].icon + '.png>')
-            weatherItem.append(day, weatherIcon, temp, humidity)
+            forecastContainer.append(day, temp, humidity)
           
-            $('#searchResultFuture').append(weatherItem)
+            $('#searchResultFuture').append(forecastContainer)
         }
 
 
